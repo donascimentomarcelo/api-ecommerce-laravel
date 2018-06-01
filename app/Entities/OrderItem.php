3c5dcf94de08326dbf5b4951084440d375e2f06f
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-06-01 11:08:03 -0300

    Criando a migracao de order e order item

---
+++ app/Entities/OrderItem.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
+
+/**
+ * Class OrderItem.
+ *
+ * @package namespace App\Entities;
+ */
+class OrderItem extends Model implements Transformable
+{
+    use TransformableTrait;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'product_id',
+        'order_id',
+        'price',
+        'qtd'
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+}
