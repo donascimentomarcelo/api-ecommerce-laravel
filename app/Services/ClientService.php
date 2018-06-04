a9db444e7d834628adebf8ea88a3ea7ce92fc262
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-06-04 01:00:03 -0300

    Ajustando relacionamento no clientService

--- app/Services/ClientService.php
+++ app/Services/ClientService.php
@@ -1,20 +1,24 @@
 <?php
 
 namespace App\Services;
+use \App\Repositories\UserRepository;
 use \App\Repositories\ClientRepository;
+use \App\Entities\User;
 use \App\Entities\Client;
 
 class ClientService 
 {
+    private $userRepository;
     private $clientRepository;
 
-    public function __construct(ClientRepository $clientRepository)
+    public function __construct(UserRepository $userRepository, ClientRepository $clientRepository)
     {
+        $this->userRepository = $userRepository;
         $this->clientRepository = $clientRepository;
     }
 
     public function list()
     {
-        return $this->clientRepository->with(['user'])->paginate(10);
+        return $this->userRepository->with(['client'])->paginate(10);
     }
 }
